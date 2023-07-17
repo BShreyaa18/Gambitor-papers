ad829d7ed86d945fb0b5c80676d6480b4bd80cf7
shreya bajpai <shreya08052004@gmail.com>
2023-07-17 14:08:50 +0530

    first commit

--- src/components/Papers.js
+++ src/components/Papers.js
@@ -1,28 +1,38 @@
 import React from "react"
 import img from "../images/Paper.png"
 import listimg from "../images/pokeball (1) 1.png"
+import { BrowserRouter , Link } from 'react-router-dom';
+import inceptionmetiox from "../PracticePapers/9th 10th COMPLETE.pdf";
+import inceptionappolox from "../PracticePapers/11TH COMPLETE.pdf";
+import inceptionathenox from "../PracticePapers/12th COMPLETE.pdf";
+import pinnaclemetiox from "../PracticePapers/Metiox Paper-1.pdf";
+import pinnacleapollox from "../PracticePapers/Metiox Paper-1.pdf";
+import pinnacleathenox from "../PracticePapers/Metiox Paper-1.pdf";
 function Papers(){
     return(
+        <BrowserRouter>
         <div className="container">
             <img src={img} className="paperimg" />
             <div className="papercontent">
                 <div className="shiftbox"><div className="box"><h6>Inception Round</h6></div></div>
                 <ul>
-                    <li><span><img src={listimg}  className="listimg" /></span>Metiox</li>
-                    <li><span><img src={listimg} className="listimg" /></span>Apollox</li>
-                    <li><span><img src={listimg} className="listimg" /></span>Athenox</li>
+                    <li><Link to={inceptionmetiox} ><span><img src={listimg}  className="listimg" /></span>Metiox</Link></li>
+                    <li><Link to={inceptionappolox} ><span><img src={listimg} className="listimg" /></span>Apollox</Link></li>
+                    <li><Link to={inceptionathenox} ><span><img src={listimg} className="listimg" /></span>Athenox</Link></li>
                 </ul>
             </div>
             <div className="papercontent">
             <div className="shiftbox"><div className="box"><h6>Pinnacle Round</h6></div></div>
                 <ul>
-                    <li><span><img src={listimg}  className="listimg" /></span>Metiox</li>
-                    <li><span><img src={listimg}  className="listimg" /></span>Apollox</li>
-                    <li><span><img src={listimg}  className="listimg" /></span>Athenox</li>
+                    <li><Link to={pinnaclemetiox} ><span><img src={listimg}  className="listimg" /></span>Metiox</Link></li>
+                    <li><Link to={pinnacleapollox} ><span><img src={listimg} className="listimg" /></span>Apollox</Link></li>
+                    <li><Link to={pinnacleathenox} ><span><img src={listimg} className="listimg" /></span>Athenox</Link></li>
                 </ul>
             </div>
 
         </div>
+        </BrowserRouter>
     );
+
 }
 export default Papers;
